fe5c926b167c17cd03510c2463c10fe30a893966
Saikrishna Rao <krish98.sai@gmail.com>
2016-11-08 12:31:35 -0800

    Started code for TicTacToePvP. - Listed procedure - Listed methods - Created some methods Created info file for all classes.

--- src/TicTacToePvP.java
+++ src/TicTacToePvP.java
@@ -2,5 +2,44 @@
  * Created by krish98sai on 11/8/2016.
  */
 public class TicTacToePvP {
+    public static void main(String[] args){
 
+        //Initialize variables
+        String[][] gameBoard = {
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+                {"-", "-", "-"},
+        };
+        String playerMark = "O";
+        boolean win = false;
+
+        while(!win){
+            printBoard(gameBoard);
+            gameBoard = playTurn(gameBoard, playerMark);
+            checkWin(gameBoard);
+            playerMark = switchPlayer(playerMark);
+        }
+
+        //Announce winner.
+        System.out.println("Player " + playerMark + " wins!");
+    }
+
+    public static void printBoard(String[][] board){
+
+    }
+
+    public static String[][] playTurn(String[][] board, String player){
+        return board;
+    }
+
+    public static void checkWin(String[][] board){
+
+    }
+
+    public static String switchPlayer(String player){
+        if(player.equals("X"))
+            return "O";
+        else
+            return "X";
+    }
 }
